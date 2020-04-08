c8951fdef60071523928d8dddc7c9872037b42e1
Alex Dinh <alexdinh1028@gmail.com>
2020-04-08 11:45:47 -0700

    working play button

---
+++ Assets/_Scripts/LoadOnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOnClick : MonoBehaviour {
+    public GameObject loadingImage;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void LoadScene(int level) {
+        loadingImage.SetActive(true);
+        Application.LoadLevel(level);
+    }
+}
