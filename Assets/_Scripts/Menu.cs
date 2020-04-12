765b00f6ce78769b504d7187eea50dd771a47dbf
Alex Dinh <alexdinh1028@gmail.com>
2020-04-12 15:46:43 -0700

    mp3/wav/aif/etc filetypes working

--- Assets/_Scripts/Menu.cs
+++ Assets/_Scripts/Menu.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEngine.Networking;
 
 public class Menu : MonoBehaviour {
     public static string path = "";
+    public AudioSource src;
+    public AudioClip clip;
+
+    public void Awake() {
+        src = gameObject.AddComponent<AudioSource>();
+    }
+
     public void Play() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadFile() {
-        // path = EditorUtility.OpenFilePanel("Viz: Load Audio File", "", "");
+        path = EditorUtility.OpenFilePanel("Viz: Load Audio File", "", "");
+        // path = "";
+        StartCoroutine(PlaySong2(path));
     }
+
+    public IEnumerator PlaySong(string path) {
+        /* Play anything non *.mp3 audio file */
+        path = "file://" + path;
+
+        UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.AIFF);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError) {
+            Debug.LogError(www.error);
+        }
+
+        AudioClip audio = DownloadHandlerAudioClip.GetContent(www);
+        src.PlayOneShot(audio);
+    }
+
+    public IEnumerator PlaySong2(string path) {
+        /* Play *.mp3 audio files */
+        path = "file://" + path;
+
+        WWW www = new WWW(path);
+        yield return www;
+
+        clip = NAudioPlayer.FromMp3Data(www.bytes);
+
+        src.PlayOneShot(clip);
+    }
+
 }
--- Assets/_Scripts/UnityWindowsFileDragAndDrop/FileDragAndDrop.cs
+++ Assets/_Scripts/UnityWindowsFileDragAndDrop/FileDragAndDrop.cs
@@ -2,9 +2,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using B83.Win32;
+using System.Collections;
+using UnityEngine.Networking;
 
 public class FileDragAndDrop : MonoBehaviour {
     List<string> log = new List<string>();
+    public static string path = "";
+    public AudioSource src;
+    public AudioClip clip;
+    private float progress;
+
     void OnEnable () {
         // must be installed on the main thread to get the right thread id.
         UnityDragAndDropHook.InstallHook();
@@ -14,13 +21,50 @@ public class FileDragAndDrop : MonoBehaviour {
         UnityDragAndDropHook.UninstallHook();
     }
 
-    void OnFiles(List<string> aFiles, POINT aPos) {
+    void OnFiles0(List<string> aFiles, POINT aPos) {
         // do something with the dropped file names. aPos will contain the 
         // mouse position within the window where the files has been dropped.
         string str = "Dropped " + aFiles.Count + " files at: " + aPos + "\n\t" +
             aFiles.Aggregate((a, b) => a + "\n\t" + b);
         Debug.LogError(str);
         log.Add(str);
+    }
+
+    void OnFiles(List<string> aFiles, POINT aPos) {
+        // do something with the dropped file names. aPos will contain the 
+        // mouse position within the window where the files has been dropped.
+        // string str = "Dropped " + aFiles.Count + " files at: " + aPos + "\n\t" +
+        //     aFiles.Aggregate((a, b) => a + "\n\t" + b);
+
+        //string path = aFiles.Aggregate((a, b) => a + "\n\t" + b);
+        string path = aFiles[0];
+        PlaySong(path);
+
+        Debug.LogError(path);
+        log.Add(path);
+    }
+
+    public IEnumerator PlaySong(string path) {
+        path = "file:///" + path;
+        Debug.LogError("playing song...");
+
+        UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.MPEG);
+        var loadingOp = www.SendWebRequest();
+
+
+        while (!www.isDone) {
+            progress = loadingOp.progress;
+            yield return null;
+        }
+
+        progress = 1f;
+
+        if (!string.IsNullOrEmpty(www.error)) {
+            // error occurred
+        }
+
+        var audio = DownloadHandlerAudioClip.GetContent(www);
+        src.PlayOneShot(audio);
     }
 
     private void OnGUI() {
