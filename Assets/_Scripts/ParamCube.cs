525b68162297b03451264457a62c08f05f14b6a5
Alex Dinh <alexdinh1028@gmail.com>
2020-06-22 01:46:29 -0700

    limited bar height

--- Assets/_Scripts/ParamCube.cs
+++ Assets/_Scripts/ParamCube.cs
@@ -6,6 +6,7 @@ public class ParamCube : MonoBehaviour {
     // Start is called before the first frame update
     public int _band;
     public float _startScale, _scaleMultiplier;
+    public float scale;
     
     void Start() {
         
@@ -14,6 +15,7 @@ public class ParamCube : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         // transform.localScale = new Vector3(transform.localScale.x, (Audio._freqBands[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
-        transform.localScale = new Vector3(transform.localScale.x, (Audio._freqBands32[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+        scale = Mathf.Min(12, _scaleMultiplier);
+        transform.localScale = new Vector3(transform.localScale.x, (Audio._freqBands32[_band] * scale) + _startScale, transform.localScale.z);
     }
 }
